80c9c20874c45739ec229b2efbd549a6a060d4f4
YuriyNi <nee.y@yandex.ru>
2018-10-19 13:04:42 +0300

    Первое ДЗ

---
+++ hw01_easy.py
@@ -0,0 +1,20 @@
+
+__author__ = 'Ni Yuriy '
+
+#Task1
+number = int (input ("Enter same number:  "))
+print (number)
+
+#Task2
+
+a = input("Enter number a:  ")
+b = input ("Enter number b:   ")
+
+
+#Task3
+
+answer = input("How old are you? ")
+if answer >= 18:
+    print ("access is allowed")
+else:
+    print ('Sorry, using this resource is only 18 years old')
