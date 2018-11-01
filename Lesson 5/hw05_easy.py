04faca3107bb73707fb3c7c41b8ec0b5b4019b54
YuriyNi <nee.y@yandex.ru>
2018-11-02 00:04:45 +0300

    Мучаю задание 3

--- Lesson 5/hw05_easy.py
+++ Lesson 5/hw05_easy.py
@@ -8,7 +8,7 @@
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
 
-import os
+import os, shutil
 """
 dir_many = ("dir_1","dir_2","dir_3","dir_4","dir_5","dir_6","dir_7","dir_8","dir_9")
 for dir1 in dir_many:
@@ -38,11 +38,17 @@ print(os.listdir())
 """
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
-"""
 
 
+dir_new1 = os.path.join(os.getcwdb(), "copy")
+try:
+    os.mkdir(dir_new1)
 
-shutil.copyfile (dir_path, dir_path1 )
+except FileExistsError:
+    print("Такая Дир уже есть")
 
-"""
 
+dir_new = os.path.join(os.getcwdb())
+
+
+shutil.copyfile (dir_new, dir_new1)
