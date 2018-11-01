ecbf9390c711a122e232d9fdffbb311b236fa0ff
YuriyNi <nee.y@yandex.ru>
2018-11-01 22:58:53 +0300

    Сделал задание 2

--- Lesson 5/hw05_easy.py
+++ Lesson 5/hw05_easy.py
@@ -1,14 +1,40 @@
+
+
+
+
+
 # Задача-1:
 # Напишите скрипт, создающий директории dir_1 - dir_9 в папке,
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
 
+import os
+
+dir_path = os.path.join (os.getcwd(), "dir_1", "dir_2", "dir_3")
+try:
+    os.mkdir(dir_path)
+except FileExistsError:
+    print("Така дир уже существует")
+
+
+"""
+
+dir_path = os.path.join (os.getcwd(), "NEW DIR"  )   
+os.rmdir(dir_path)   # Удаление директории
+
+"""
+
+
+
+
+
 
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
-
-
-
+"""
+dir_path = os.path.join (os.getcwd())
+print(os.listdir(dir_path))
+"""
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
 
