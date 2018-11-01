10df5fc7e86b7daf315a34cf6f8d0cc462e21777
YuriyNi <nee.y@yandex.ru>
2018-11-01 23:44:23 +0300

    Улучшил задание 2

--- Lesson 5/hw05_easy.py
+++ Lesson 5/hw05_easy.py
@@ -15,12 +15,12 @@ for dir1 in dir_many:
     os.makedirs (os.path.join (dir1))
 
 
-
+"""
 """
 dir_many = ("dir_1","dir_2","dir_3","dir_4","dir_5","dir_6","dir_7","dir_8","dir_9")
 for dir1 in dir_many:
     os.rmdir (os.path.join (dir1))
- 
+"""
 
 
 
@@ -34,8 +34,7 @@ for dir1 in dir_many:
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
 """
-dir_path = os.path.join (os.getcwd())
-print(os.listdir(dir_path))
+print(os.listdir())
 """
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
@@ -46,3 +45,4 @@ print(os.listdir(dir_path))
 shutil.copyfile (dir_path, dir_path1 )
 
 """
+
