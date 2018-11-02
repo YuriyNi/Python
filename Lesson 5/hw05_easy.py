b4962d96dfd25eea9559ef4f8fb842e8ee8129aa
YuriyNi <nee.y@yandex.ru>
2018-11-02 22:03:08 +0300

    Задание 3

--- Lesson 5/hw05_easy.py
+++ Lesson 5/hw05_easy.py
@@ -7,9 +7,9 @@
 # Напишите скрипт, создающий директории dir_1 - dir_9 в папке,
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
-
-import os, shutil
 """
+import os, shutil
+
 dir_many = ("dir_1","dir_2","dir_3","dir_4","dir_5","dir_6","dir_7","dir_8","dir_9")
 for dir1 in dir_many:
     os.makedirs (os.path.join (dir1))
@@ -39,16 +39,15 @@ print(os.listdir())
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
 
-
-dir_new1 = os.path.join(os.getcwdb(), "copy")
+import os, shutil
+dir_new1 = os.path.join(os.getcwdb(),"Copy")
 try:
     os.mkdir(dir_new1)
 
 except FileExistsError:
     print("Такая Дир уже есть")
+dir_new = os.path.join(os.getcwdb()
+
+shutil.copy2 (dir_new, dir_new1)
 
 
-dir_new = os.path.join(os.getcwdb())
-
-
-shutil.copyfile (dir_new, dir_new1)
