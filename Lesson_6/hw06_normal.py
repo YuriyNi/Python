b688c23f69538c8a0bba9b242a0dac6272563aca
YuriyNi <nee.y@yandex.ru>
2018-11-07 13:53:59 +0300

    Начал задание

--- Lesson_6/hw06_normal.py
+++ Lesson_6/hw06_normal.py
@@ -16,3 +16,18 @@
 # 4. Узнать ФИО родителей указанного ученика
 # 5. Получить список всех Учителей, преподающих в указанном классе
 
+class schoollboy:
+   def __init__(self, name, surname, class_, subject):
+       self.name = name
+       self.surname = surname
+       self.class_ = class_
+       self.subject = subject
+       
+
+
+class parents:
+    def __init__(self, name, surname, children)
+
+
+
+class teacher:
