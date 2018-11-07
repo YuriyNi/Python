77d96c36232afe562f676df2f85991f88097647c
YuriyNi <nee.y@yandex.ru>
2018-11-07 09:58:21 +0300

    Сделал задание 2

--- Lesson_6/hw06_easy.py
+++ Lesson_6/hw06_easy.py
@@ -10,17 +10,9 @@ class Tetra:
         self.y2 = y2
         self.x3 = x3
         self.y3 = y3
-
-
-
-    def AB(self):
-        return math.sqrt((self.x1 - self.x2) ** 2 + (self.y1 - self.y2) ** 2)
-
-    def BC(self):
-        return math.sqrt((self.x2-self.x3)** 2 + (self.y2 - self.y3) ** 2)
-
-    def AC(self):
-        return math.sqrt((self.x3 - self.x1) ** 2 + (self.y3 - self.y1) ** 2)
+        self.AB = math.sqrt((self.x1 - self.x2) ** 2 + (self.y1 - self.y2) ** 2)
+        self.BC = math.sqrt((self.x2-self.x3) ** 2 + (self.y2 - self.y3) ** 2)
+        self.AC = math.sqrt((self.x3 - self.x1) ** 2 + (self.y3 - self.y1) ** 2)
 
     def perimeter(self):
         return self.AB + self.BC + self.AC
@@ -49,3 +41,39 @@ class Tetra:
 # проверка, является ли фигура равнобочной трапецией;
 # вычисления: длины сторон, периметр, площадь.
 
+class Trapeze:
+    def __init__(self, A, B, C, D):
+
+        def sideLen(dot1, dot2):
+            return math.sqrt((dot1[0] - dot2[0]) ** 2
+                             + (dot1[1] - dot2[1]) ** 2)
+
+        def areaTriangle(len1, len2, len3):
+            semi_perimeter = (len1 + len2 + len3) / 2
+
+            return math.sqrt(semi_perimeter
+                             * (semi_perimeter - len1)
+                             * (semi_perimeter - len2)
+                             * (semi_perimeter - len3))
+
+        self.A = A
+        self.B = B
+        self.C = C
+        self.D = D
+
+        self.AB = sideLen(self.A, self.B)
+        self.BC = sideLen(self.B, self.C)
+        self.CD = sideLen(self.C, self.D)
+        self.DA = sideLen(self.D, self.A)
+        self.diagonal_AC = sideLen(self.C, self.A)
+        self.diagonal_BD = sideLen(self.B, self.D)
+        self.perimeter = self.AB + self.BC + self.CD + self.DA
+
+
+        self.area = areaTriangle(self.AB, self.diagonal_BD, self.DA) \
+                    + areaTriangle(self.diagonal_BD, self.BC, self.CD)
+
+    def isTrapezeEqu(self):
+        if self.diagonal_AC == self.diagonal_BD:
+            return True
+        return False
