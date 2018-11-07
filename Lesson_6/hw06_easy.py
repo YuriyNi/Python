ca584b33493f5da16f29a897b45a7bececa5eac1
YuriyNi <nee.y@yandex.ru>
2018-11-07 09:32:53 +0300

    Сделал заданик 1

--- Lesson_6/hw06_easy.py
+++ Lesson_6/hw06_easy.py
@@ -1,15 +1,45 @@
 # Задача-1: Написать класс для фигуры-треугольника, заданного координатами трех точек.
 # Определить методы, позволяющие вычислить: площадь, высоту и периметр фигуры.
+import math
 
-class tetra:
-    def __init__(self, point1, point2, point3):
-        self.point1 = point1
-        self.point2 = point2
-        self.point3 = point3
-    
+class Tetra:
+    def __init__(self, x1,y1,x2,y2,x3,y3):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        self.x3 = x3
+        self.y3 = y3
+
+
+
+    def AB(self):
+        return math.sqrt((self.x1 - self.x2) ** 2 + (self.y1 - self.y2) ** 2)
+
+    def BC(self):
+        return math.sqrt((self.x2-self.x3)** 2 + (self.y2 - self.y3) ** 2)
+
+    def AC(self):
+        return math.sqrt((self.x3 - self.x1) ** 2 + (self.y3 - self.y1) ** 2)
+
+    def perimeter(self):
+        return self.AB + self.BC + self.AC
+
 
     def square (self):
-        self.AB =
+        semi_perimeter = self.perimeter() / 2
+        return math.sqrt(semi_perimeter
+                         * (semi_perimeter - self.AB)
+                         * (semi_perimeter - self.BC)
+                         * (semi_perimeter - self.CA))
+
+    def height(self):
+        return self.square() / (self.AB / 2)
+
+
+
+
+
 
 
 
